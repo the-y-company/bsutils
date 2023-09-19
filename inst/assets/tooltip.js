f77b6301ba3f9633880b59ca07301006156a6775
John Coene <jcoenep@gmail.com>
2023-09-20 00:32:59 +0200

    fix: tooltip

--- inst/assets/tooltip.js
+++ inst/assets/tooltip.js
@@ -1,8 +1,8 @@
-$(document).on('shiny:connected', () => {
-  setTimeout(() => {
-    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
-    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-      return new bootstrap.Tooltip(tooltipTriggerEl)
-    });
-  }, 250);
-});
+setTimeout(() => {
+  var tooltipTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="tooltip"]'),
+  );
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+  });
+}, 150);

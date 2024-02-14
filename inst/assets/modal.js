fca7bdf43297a1ec713ff6827cef9bc746d50c50
masierom <mauromiguelm@hotmail.com>
2024-02-14 16:52:59 +0100

    FIx minor bug in modal.js and remove test.R

--- inst/assets/modal.js
+++ inst/assets/modal.js
@@ -1,18 +1,18 @@
-Shiny.addCustomMessageHandler("bsutils-modal", (msg) => {
+Shiny.addCustomMessageHandler('bsutils-modal', (msg) => {
   var modal = bootstrap.Modal.getOrCreateInstance(
     document.getElementById(msg.id),
     {},
   );
 
-  if (msg.action == "show") {
+  if(msg.action == 'show'){
     modal.show();
     return;
   }
 
-  if (msg.action == "hide") {
+  if(msg.action == 'hide'){
     modal.hide();
     return;
   }
-
+  
   modal.toggle();
 });
